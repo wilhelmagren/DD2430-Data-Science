51343bf34d48629a893e48de8e8be540b006776e
ulysses@bund-subsystem <wagren@kth.se>
2021-11-03 19:09:04 +0100

    reformating

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,18 @@
 """
-as of writing this I have realised that I wasted 6 hours,
-beacuse I thought the from braindecode.datautil.windowers.create_fixed_length_windows()
-function didn't work using our MEG data since we don't have any events. while all along there is 
-a faulty raw.fif file in our dataset. namely the file: sub-01_ses-psd_task-rest_eo_ds_raw.fif
+File implements ulitity functions used between various scripts.
+Contains default variables in all caps, add more if necessary.
 
-anyway. so here is a bunch of code I wrote for working around the braindecode and mne.Epochs
-dependencies and only resorting to pytorch Dataset and ConcatDataset. this code works good
-also but there is no need for it maybe. or maybe it is. we will see how the other
-pipeline works with the MEG data.
+
+Author: Wilhelm Ågren, wagren@kth.se
+Last edited: 03-11-2021
 """
 import os
-import mne
-import torch
-import random
-import numpy as np
-import pandas as pd
-
-from torch.utils.data import Dataset, ConcatDataset, Sampler
-from sklearn.model_selection import LeavePGroupsOut
-
 
 RELATIVE_DIRPATH = '../data/data-ds-200HZ/'
 STATEID_MAP = {1: 'ses-con_task-rest_ec',
                2: 'ses-con_task-rest_eo',
                3: 'ses-psd_task-rest_ec',
                4: 'ses-psd_task-rest_eo'}
-
-
-
 
 def WPRINT(msg, instance):
     print("[*]  {}\t{}".format(str(instance), msg)) if instance._verbose else None
