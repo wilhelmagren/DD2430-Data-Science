f9b68b93ba286ebee70067f3d82b6594049b22fe
ulysses@bund-subsystem <wagren@kth.se>
2021-11-03 19:11:15 +0100

    added utility functions for _fetch_data func

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,33 @@ def WPRINT(msg, instance):
 def EPRINT(msg, instance):
     print("[!]  {}\t{}".format(str(instance), msg))
 
+def get_subject_id(filepath):
+    return filepath.split('_')[0].split('-')[-1]
+
+def get_recording_id(filepath):
+    if 'ses-con_task-rest_ec' in filepath:
+        return 1
+    if 'ses-con_task-rest_eo' in filepath:
+        return 2
+    if 'ses-psd_task-rest_ec' in filepath:
+        return 3
+    if 'ses-psd_task-rest_eo' in filepath:
+        return 4
+    raise ValueError
+
+def get_subject_gender(f):
+    id = get_subject_id(f)
+    with open('../data/subjects.tsv', 'r') as fil:
+        for line in fil.readlines():
+            if id in line:
+                return 0 if line.split('\t')[2] == 'F' else 1
+    raise ValueError
+
+def get_subject_age(f):
+    id = get_subject_id(f)
+    with open('../data/subjects.tsv', 'r') as fil:
+        for line in fil.readlines():
+            if id in line:
+                return int(line.split('\t')[1])
+    raise ValueError
+
